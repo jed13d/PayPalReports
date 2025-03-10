ff89046e9cd2a1e0b7bf10f18a102316bc801936
Justin Dowell <j3.dowell@gmail.com>
2025-03-10 19:39:03 -0400

    Data model for PayPal token request response

---
+++ PayPalReports/DataModels/PayPalTokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PayPalReports.DataModels
+{
+    record class PayPalTokenResponse(
+        [property: JsonPropertyName("scope")] string scope,
+        [property: JsonPropertyName("access_token")] string access_token,
+        [property: JsonPropertyName("token_type")] string token_type,
+        [property: JsonPropertyName("app_id")] string app_id,
+        [property: JsonPropertyName("expires_in")] string expires_in,
+        [property: JsonPropertyName("nonce")] string nonce)
+    {
+    }
+}
