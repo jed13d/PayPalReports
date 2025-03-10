783d6f71eee08f5208be9b4f29e5696762b87626
Justin Dowell <j3.dowell@gmail.com>
2025-03-10 19:36:44 -0400

    Custom navigation tab for app

---
+++ PayPalReports/NavTab.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace PayPalReports
+{
+    class NavTab : ListBoxItem
+    {
+        public static readonly DependencyProperty NavLinkProperty = DependencyProperty.Register("NavLink", typeof(Uri), typeof(NavTab), new PropertyMetadata(null));
+
+        static NavTab()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavTab), new FrameworkPropertyMetadata(typeof(NavTab)));
+        }
+
+        public Uri NavLink
+        {
+            //get { return new Uri(GetValue(NavLinkProperty), UriKind.Relative);  }
+            get { return (Uri)GetValue(NavLinkProperty); }
+            set
+            {
+                SetValue(NavLinkProperty, value);
+            }
+        }
+    }
+}
