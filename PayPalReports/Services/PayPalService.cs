158550f37eb1759b5bf8177cb4f48fd109e43cf3
Justin Dowell <j3.dowell@gmail.com>
2025-03-11 09:01:22 -0400

    Have a working call to PalPal for acquiring a bearer token

---
+++ PayPalReports/Services/PayPalService.cs
@@ -0,0 +1,116 @@
+﻿using PayPalReports.DataModels;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace PayPalReports.Services
+{
+    class PayPalService
+    {
+        private string[] _apiData = new string[6];
+        private PayPalTokenResponse? _tokenData;
+
+        private readonly string PAYPAL_DATA_FILE = "pdata.dat";
+
+        private readonly int URL = 0;
+        private readonly int REGION = 1;
+        private readonly int EMAIL = 2;
+        private readonly int PASSWORD = 3;
+        private readonly int ID = 4;
+        private readonly int KEY = 5;
+
+        private readonly string PAYPAL_TRANSACTION_HISTORY_ENDPOINT = "/v1/reporting/transactions";
+        /*      * required
+         * start_date   * (f/e)     -   string [20 - 64] characters         internet date/time format  yyyy-mm-ddThh:mm:ss
+         * end_date     * (f/e)     -   ''
+         * fields        (backend)  -   string  desired - "transaction_info,payer_info"
+         * page_size    (be)        -   int     1-500 (default-100)
+         * page         (be)        -   int     default 1
+         * */
+        private readonly string PAYPAL_BALANCE_ENDPOINT = "/v1/reporting/balances";
+        /*  requires TOKEN
+         *  as_of_time              -   string [20 - 64] characters         internet date/time format  yyyy-mm-ddThh:mm:ss
+         * 
+         * */
+
+        private readonly string PAYPAL_OAUTH_TOKEN_ENDPOINT = "/v1/oauth2/token";
+
+        public PayPalService()
+        {
+            LoadApiInfo();
+        }
+
+        public async Task<bool> TestTokenRequest()
+        {
+            bool returnValue = false;
+
+            if (_tokenData == null)     // if the token exists, no reason to get a new one
+            {
+                await RequestToken();
+            }
+
+            if (_tokenData != null)
+            {
+                returnValue = true;
+            }
+
+            return returnValue;
+        }
+
+
+        private void LoadApiInfo()
+        {
+            DataEncryptionService des = new();
+
+            string fileData = des.RetrieveData(PAYPAL_DATA_FILE);
+
+            _apiData = fileData.Split('\n');
+        }
+
+        private async Task RequestToken()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Setup Headers
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en_US"));
+
+                var base64encoded = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_apiData[ID]}:{_apiData[KEY]}"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64encoded);
+
+                // Setup Content Data
+                var keyValues = new List<KeyValuePair<string, string>>();
+                keyValues.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+
+                // Send POST request 
+                string token_uri = _apiData[URL] + PAYPAL_OAUTH_TOKEN_ENDPOINT;
+                var responseMessage = await client.PostAsync(token_uri, new FormUrlEncodedContent(keyValues));
+
+                // Stream the responseMessage through Json Deserializer, converting to usable object
+                var tokenData = await JsonSerializer.DeserializeAsync<PayPalTokenResponse>(responseMessage.Content.ReadAsStream());
+
+                if (tokenData != null)
+                {
+                    _tokenData = tokenData;
+                }
+                else
+                {
+                    Debug.WriteLine($"Failed to get token from {token_uri}");
+                }
+
+                /*      Sample Response for token
+                    {
+                      "scope": "https://uri.paypal.com/services/invoicing https://uri.paypal.com/services/disputes/read-buyer https://uri.paypal.com/services/payments/realtimepayment https://uri.paypal.com/services/disputes/update-seller https://uri.paypal.com/services/payments/payment/authcapture openid https://uri.paypal.com/services/disputes/read-seller https://uri.paypal.com/services/payments/refund https://api-m.paypal.com/v1/vault/credit-card https://api-m.paypal.com/v1/payments/.* https://uri.paypal.com/payments/payouts https://api-m.paypal.com/v1/vault/credit-card/.* https://uri.paypal.com/services/subscriptions https://uri.paypal.com/services/applications/webhooks",
+                      "access_token": "A21AAFEpH4PsADK7qSS7pSRsgzfENtu-Q1ysgEDVDESseMHBYXVJYE8ovjj68elIDy8nF26AwPhfXTIeWAZHSLIsQkSYz9ifg",
+                      "token_type": "Bearer",
+                      "app_id": "APP-80W284485P519543T",
+                      "expires_in": 31668,
+                      "nonce": "2020-04-03T15:35:36ZaYZlGvEkV4yVSz8g6bAKFoGSEzuy3CQcz3ljhibkOHg"
+                    }
+                * */
+            }
+        }
+    }
+}
