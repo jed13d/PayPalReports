294b014d371cbd28806ffeed2bb8a722953d265c
Justin Dowell <j3.dowell@gmail.com>
2025-03-12 23:26:46 -0400

    Preparing for transactions request from api

---
+++ PayPalReports/DataModels/PayPalReportDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace PayPalReports.DataModels
+{
+    class PayPalReportDetails
+    {
+        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
