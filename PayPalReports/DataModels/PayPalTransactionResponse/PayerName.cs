809c634433cad0cabd8f0669ec420c02193ed97a
Justin Dowell <j3.dowell@gmail.com>
2025-03-12 23:27:52 -0400

    Preparing for transactions request from api

---
+++ PayPalReports/DataModels/PayPalTransactionResponse/PayerName.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PayPalReports.DataModels.PayPalTransactionResponse
+{
+    record class PayerName(
+        [property: JsonPropertyName("given_name")] string given_name,
+        [property: JsonPropertyName("surname")] string surname,
+        [property: JsonPropertyName("full_name")] string full_name,
+        [property: JsonPropertyName("alternate_full_name")] string alternate_full_name)
+    {
+    }
+}
