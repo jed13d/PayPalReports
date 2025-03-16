3b023b5da8c3ba7f38155fe6590fc6c210d5d3c9
Justin Dowell <j3.dowell@gmail.com>
2025-03-16 00:26:29 -0400

    Preparing crash handler

---
+++ PayPalReports/Pages/CrashInfoWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PayPalReports.Pages
+{
+    /// <summary>
+    /// Interaction logic for CrashInfoWindow.xaml
+    /// </summary>
+    public partial class CrashInfoWindow : Window
+    {
+        public CrashInfoWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
