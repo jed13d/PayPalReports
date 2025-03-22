64bd8a2386101956a799d418eb3c0bdb247504f1
Justin Dowell <j3.dowell@gmail.com>
2025-03-22 15:04:40 -0400

    Created models in preparation for Google API interface (service)

---
+++ PayPalReports/DataModels/GoogleAPI/GoogleApiCredInstalledType.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PayPalReports.DataModels.GoogleAPI
+{
+    record class GoogleApiCredInstalledType(
+        [property: JsonPropertyName("installed")] GoogleApiCreds installed)
+    {
+    }
+}
