bd9918c54645996ec9186152f6e93f5d216efc5e
Justin Dowell <j3.dowell@gmail.com>
2025-03-27 16:26:27 -0400

    Creating service for outputting to excel file instead of google sheets api

---
+++ PayPalReports/DataModels/ExcelReportContext.cs
@@ -0,0 +1,11 @@
+﻿using PayPalReports.DataModels.PayPalAPI;
+
+namespace PayPalReports.DataModels
+{
+    internal class ExcelReportContext
+    {
+        public PayPalReportDetails? PayPalReportDetails { get; set; }
+
+        public string? OutputPath { get; set; }
+    }
+}
---
+++ PayPalReports/Services/ExcelService.cs
@@ -0,0 +1,38 @@
+﻿using EasyXLS;
+
+namespace PayPalReports.Services
+{
+    internal class ExcelService
+    {
+        public ExcelService()
+        {
+            SampleGenerateWorkbook();
+        }
+
+        private void SampleGenerateWorkbook()
+        {
+            // Create an instance of the class that creates Excel files, having multiple sheets
+            ExcelDocument workbook = new ExcelDocument(2);
+
+            // Set the sheet names
+            workbook.easy_getSheetAt(0).setSheetName("First tab");
+            workbook.easy_getSheetAt(1).setSheetName("Second tab");
+
+            // Create a worksheet
+            workbook.easy_addWorksheet("First tab");
+
+            // Create Excel file
+            workbook.easy_WriteXLSXFile("C:\\Samples\\Excel file.xlsx");
+
+            // Confirm the creation of the Excel file
+            String sError = workbook.easy_getError();
+            if (sError.Equals(""))
+                Console.Write("\nFile successfully created. Press Enter to Exit...");
+            else
+                Console.Write("\nError encountered: " + sError + "\nPress Enter to Exit...");
+
+            // Dispose memory
+            workbook.Dispose();
+        }
+    }
+}
