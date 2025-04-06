b629e3df964708e532c429dc8f1b849634c9e6ef
Justin Dowell <j3.dowell@gmail.com>
2025-04-05 21:41:14 -0400

    Added constructor

--- PayPalReports/DataModels/ExcelReportContext.cs
+++ PayPalReports/DataModels/ExcelReportContext.cs
@@ -4,8 +4,14 @@ namespace PayPalReports.DataModels
 {
     internal class ExcelReportContext
     {
-        public PayPalReportDetails? PayPalReportDetails { get; set; }
+        public PayPalReportDetails PayPalReportDetails { get; set; }
 
-        public string? OutputPath { get; set; }
+        public string OutputPath { get; set; }
+
+        public ExcelReportContext(PayPalReportDetails payPalReportDetails, string outputPath)
+        {
+            PayPalReportDetails = payPalReportDetails;
+            OutputPath = outputPath;
+        }
     }
 }
