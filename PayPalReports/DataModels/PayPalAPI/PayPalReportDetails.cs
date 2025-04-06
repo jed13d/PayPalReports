ddd411b130c34c449349b0452321ad1cb0d5e042
Justin Dowell <j3.dowell@gmail.com>
2025-04-05 21:41:54 -0400

    Changed method of getting ISO datetime string

--- PayPalReports/DataModels/PayPalAPI/PayPalReportDetails.cs
+++ PayPalReports/DataModels/PayPalAPI/PayPalReportDetails.cs
@@ -5,16 +5,26 @@ namespace PayPalReports.DataModels.PayPalAPI
 {
     class PayPalReportDetails
     {
-        public string EndDate { get; set; }
-        public string StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; }
         public TransactionResponse? PayPalTransactionResponse { get; set; }
         public BalanceResponse? PayPalEndBalanceResponse { get; set; }
         public BalanceResponse? PayPalStartBalanceResponse { get; set; }
 
+        private readonly string ISO_DATE_TIME_FORMAT = "yyyy-MM-ddTHH:mm:sszzz";
+
         public PayPalReportDetails()
         {
-            EndDate = "";
-            StartDate = "";
+        }
+
+        public string GetEndDateISO()
+        {
+            return EndDate.ToString(ISO_DATE_TIME_FORMAT, System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        public string GetStartDateISO()
+        {
+            return StartDate.ToString(ISO_DATE_TIME_FORMAT, System.Globalization.CultureInfo.InvariantCulture);
         }
     }
 }
