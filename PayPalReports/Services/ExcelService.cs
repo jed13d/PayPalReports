602d92e4ac5d4ee64faf6d2ece1f39663c696a64
Justin Dowell <j3.dowell@gmail.com>
2025-05-18 11:27:57 -0400

    Huge update to Excel output.

--- PayPalReports/Services/ExcelService.cs
+++ PayPalReports/Services/ExcelService.cs
@@ -22,28 +22,35 @@ namespace PayPalReports.Services
         private readonly string OPENING_BALANCE_HEADER_CELL = "A4";
         private readonly string OPENING_BALANCE_CELL = "B4";
 
+        private readonly string HEADERS_FOR_STYLE = "A6:G7";
+        private readonly string HEADERS_FOR_AUTO_FILTER = "A6:G6";
+        private readonly string HEADER_LABEL_ROW = "6";
         private readonly string[] HEADER_STRINGS = ["Date", "Reference", "Account", "Explanation", "Debit (+)", "Credit (-)", "Balance"];
-        private readonly string[] DROPDOWN_TABLE_VALUES_C1 = ["Types of Accounts", "Petty Cash-Revenue", "Checking", "PayPal-Expenses", "PayPal-Revenue", "Petty Cash-Expenses"];
-        private readonly string[] DROPDOWN_TABLE_VALUES_C2 = ["DR/CR", "CR", "DR", "DR", "CR", "DR"];
+        private readonly string[] DROPDOWN_TABLE_VALUES_C1 = ["Types of Accounts", "PayPal-Revenue", "Petty Cash-Revenue", "Checking", "PayPal-Expenses", "Petty Cash-Expenses"];
+        private readonly string[] DROPDOWN_TABLE_VALUES_C2 = ["DR/CR", "DR", "DR", "CR", "CR", "CR"];
 
         private readonly string DEBIT_TOTAL_CELL = "E7";
         private readonly string CREDIT_TOTAL_CELL = "F7";
         private readonly string BALANCE_TOTAL_CELL = "G7";
-        private readonly string BALANCE_TOTAL_FORMULA = "=B4+E7-F7";
+        private readonly string BALANCE_TOTAL_FORMULA = "SUM(B4,E7,-F7)";
 
         private readonly string CURRENCY_FORMAT = "_($* #,##0.00_);_($* (#,##0.00);_($* \"-\"??_);_(@_)";
         private readonly string[] ALPHA = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
         private readonly string[] MONTH_STRING = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-        private readonly string ACCOUNT_DROPDOWN_VALUES_CELLS_FORMULA = "$I$10:%I%14";
+        private readonly string ACCOUNT_DROPDOWN_VALUES_CELLS_FORMULA = "$I$10:$I$14";
         private readonly int DATA_START_ROW = 8;
         private readonly string COL_DATE = "A";
+        private readonly int COL_DATE_INT = 1;
         private readonly string COL_REFERENCE = "B";
         private readonly string COL_ACCOUNT = "C";
         private readonly string COL_NOTES = "D";
-        private readonly string COL_CREDIT = "E";
-        private readonly string COL_DEBIT = "F";
+        private readonly string COL_DEBIT = "E";
+        private readonly int COL_DEBIT_INT = 5;
+        private readonly string COL_CREDIT = "F";
+        private readonly int COL_CREDIT_INT = 6;
         private readonly string COL_BALANCE = "G";
+        private readonly int COL_BALANCE_INT = 7;
 
         private readonly ILogger<ExcelService> LOGGER;
         private readonly StatusEvent STATUS_EVENT;
@@ -142,21 +149,27 @@ namespace PayPalReports.Services
                         UpdateStatusText($"Generating template worksheet.");
                         GenerateTemplate(xlsWorkbook);
                     }
+                    else
+                    {
+                        LOGGER.LogDebug("Template exists, no need to generate.");
+                    }
 
                     // some variable definitions for the data assignment loop
                     int curMonth = -1;
                     int curYear = -1;
                     int curRow = -1;
-                    string titleString = "";
+                    string titleString = string.Empty;
                     ExcelWorksheet xlsWorksheet = xlsWorkbook.Worksheets[TEMPLATE];
                     foreach (TransactionDetails transactionDetails in reportContext!.PayPalReportDetails!.PayPalTransactionResponse!.transaction_details)
-                    {
+                    {// record parsing loop
+
                         // check date is still within the current month, or move to a new sheet
                         DateTime updateDate = DateTime.Parse(transactionDetails.transaction_info.transaction_updated_date);
                         if (updateDate.Month != curMonth)
                         {
+                            #region Create New Sheet
                             // if curMonth > 0, then input balance totals, closing out the sheet before creating a new one
-                            if (curMonth > 0)
+                            if (curRow >= DATA_START_ROW && curMonth > 0)
                             {
                                 xlsWorksheet.Cells[DEBIT_TOTAL_CELL].Formula = $"=SUM(E8:E{curRow - 1};{CURRENCY_FORMAT})";
                                 xlsWorksheet.Cells[CREDIT_TOTAL_CELL].Formula = $"=SUM(F8:F{curRow - 1};{CURRENCY_FORMAT})";
@@ -164,7 +177,10 @@ namespace PayPalReports.Services
 
                             // new sheet is required
 
-                            UpdateStatusText($"Generating worksheet for {MONTH_STRING[curMonth]}.");
+                            if (curMonth >= 1 && curMonth <= 12)
+                            {
+                                UpdateStatusText($"Generating worksheet for {MONTH_STRING[curMonth]}.");
+                            }
                             // processing delay
                             Thread.Sleep(1000);
 
@@ -180,42 +196,51 @@ namespace PayPalReports.Services
 
                             // Update Title "Date"
                             xlsWorksheet.Cells[TITLE_DATE_CELL].Value = titleString;
+
+                            // Add beginning balance -- TODO modify for multiple month
+                            xlsWorksheet.Cells[OPENING_BALANCE_CELL].Formula = CURRENCY_FORMAT;
+                            xlsWorksheet.Cells[OPENING_BALANCE_CELL].Value = double.Parse(reportContext!.PayPalReportDetails!.PayPalStartBalanceResponse!.balances[0].total_balance.value);
+
+                            #endregion
                         }
 
-                        // assign data to various fields
+                        #region Record Data Assignment
 
                         // date
-                        xlsWorksheet.Cells[$"{curRow}{COL_DATE}"].Value = updateDate.Date.ToString();
+                        xlsWorksheet.Cells[$"{COL_DATE}{curRow}"].Value = updateDate;
 
                         // reference 
-                        xlsWorksheet.Cells[$"{curRow}{COL_REFERENCE}"].Value = transactionDetails.transaction_info.transaction_id;
+                        xlsWorksheet.Cells[$"{COL_REFERENCE}{curRow}"].Value = transactionDetails.transaction_info.transaction_id;
 
                         // account dropdown
-                        ExcelRange accountTypeCell = xlsWorksheet.Cells[$"{curRow}{COL_ACCOUNT}"];
-                        var dropdown = xlsWorksheet.DataValidations.AddListValidation(accountTypeCell.Address);
+                        var dropdown = xlsWorksheet.DataValidations.AddListValidation($"{COL_ACCOUNT}{curRow}");
                         dropdown.Formula.ExcelFormula = ACCOUNT_DROPDOWN_VALUES_CELLS_FORMULA;
 
                         // explanation 
-                        xlsWorksheet.Cells[$"{curRow}{COL_NOTES}"].Value = transactionDetails.transaction_info.transaction_note;
+                        xlsWorksheet.Cells[$"{COL_NOTES}{curRow}"].Value = $"{transactionDetails.payer_info.email_address} - {transactionDetails.payer_info.payer_name.given_name} - {transactionDetails.transaction_info.transaction_note}";
 
+
+                        // transaction amount (debit or credit)
                         double transactionAmount = double.Parse(transactionDetails.transaction_info.transaction_amount.value);
 
-                        // debit        // FORMAT FOR ACCOUNTING
-                        xlsWorksheet.Cells[$"{curRow}{COL_DEBIT}"].Value = transactionAmount >= 0 ? transactionAmount : 0;
-                        xlsWorksheet.Cells[$"{curRow}{COL_DEBIT}"].Formula = CURRENCY_FORMAT;
+                        // debit
+                        xlsWorksheet.Cells[$"{COL_DEBIT}{curRow}"].Formula = CURRENCY_FORMAT;
+                        xlsWorksheet.Cells[$"{COL_DEBIT}{curRow}"].Value = transactionAmount >= 0f ? transactionAmount : 0f;
 
                         // credit 
-                        xlsWorksheet.Cells[$"{curRow}{COL_CREDIT}"].Value = transactionAmount <= 0 ? Math.Abs(transactionAmount) : 0;
-                        xlsWorksheet.Cells[$"{curRow}{COL_CREDIT}"].Formula = CURRENCY_FORMAT;
+                        xlsWorksheet.Cells[$"{COL_CREDIT}{curRow}"].Formula = CURRENCY_FORMAT;
+                        xlsWorksheet.Cells[$"{COL_CREDIT}{curRow}"].Value = transactionAmount < 0f ? Math.Abs(transactionAmount) : 0f;
 
                         // balance 
-                        xlsWorksheet.Cells[$"{curRow}{COL_BALANCE}"].Value = transactionDetails.transaction_info.transaction_note;
-                        xlsWorksheet.Cells[$"{curRow}{COL_BALANCE}"].Formula = CURRENCY_FORMAT;
+                        xlsWorksheet.Cells[$"{COL_BALANCE}{curRow}"].Formula = CURRENCY_FORMAT;
+                        xlsWorksheet.Cells[$"{COL_BALANCE}{curRow}"].Value = double.Parse(transactionDetails.transaction_info.ending_balance.value);
+
+                        #endregion
 
                         // increment the current row
                         curRow++;
 
-                    }   // data assignment loop
+                    }// record parsing loop
 
                     excelPackage.Save();
                 }
@@ -256,18 +281,17 @@ namespace PayPalReports.Services
             ApplyStyleOpeningBalance(xlsWorksheet.Cells[OPENING_BALANCE_CELL]);
 
             // Setup Read of the headers
-            ApplyStyleHeader(xlsWorksheet.Cells["A6:G7"]);
+            ApplyStyleHeader(xlsWorksheet.Cells[HEADERS_FOR_STYLE]);
             for (int column = 0; column < HEADER_STRINGS.Length; column++)
             {
-                xlsWorksheet.Cells[$"{ALPHA[column]}6"].Value = HEADER_STRINGS[column];
+                xlsWorksheet.Cells[$"{ALPHA[column]}{HEADER_LABEL_ROW}"].Value = HEADER_STRINGS[column];
             }
-            xlsWorksheet.Cells[BALANCE_TOTAL_CELL].Formula = $"{BALANCE_TOTAL_FORMULA};{CURRENCY_FORMAT}";
 
             // Freeze header rows
             xlsWorksheet.View.FreezePanes(8, 1);
 
             // Add autofilter to a range of cells
-            xlsWorksheet.Cells["A6:G6"].AutoFilter = true;
+            xlsWorksheet.Cells[HEADERS_FOR_AUTO_FILTER].AutoFilter = true;
 
 
             // Setup Account dropdown table
@@ -278,15 +302,25 @@ namespace PayPalReports.Services
                 xlsWorksheet.Cells[row, 10].Value = DROPDOWN_TABLE_VALUES_C2[i];
             }
 
+            // Some field formatting
+            xlsWorksheet.Cells[DATA_START_ROW, COL_DATE_INT, xlsWorksheet.Rows.EndRow, COL_DATE_INT].Style.Numberformat.Format = "mm/dd hh:mm";
+            xlsWorksheet.Cells[DATA_START_ROW, COL_DATE_INT, xlsWorksheet.Rows.EndRow, COL_DATE_INT].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+            xlsWorksheet.Cells[DATA_START_ROW - 1, COL_DEBIT_INT, xlsWorksheet.Rows.EndRow, COL_BALANCE_INT].Style.Numberformat.Format = CURRENCY_FORMAT;
+
+            xlsWorksheet.Cells[DEBIT_TOTAL_CELL].Formula = $"SUM(E8:E{xlsWorksheet.Rows.EndRow})";
+            xlsWorksheet.Cells[CREDIT_TOTAL_CELL].Formula = $"SUM(F8:F{xlsWorksheet.Rows.EndRow})";
+            xlsWorksheet.Cells[BALANCE_TOTAL_CELL].Formula = $"{BALANCE_TOTAL_FORMULA}";
+
 
             // Set column widths
             for (int column = 1; column <= 10; column++)
             {
-                xlsWorksheet.Columns[1].Width = 20.71;
-                xlsWorksheet.Columns[2].Width = 13.57;
-                xlsWorksheet.Columns[3].Width = 25;
-                xlsWorksheet.Columns[4].Width = 35;
-                xlsWorksheet.Columns[5, 8].Width = 13.57;
+                xlsWorksheet.Columns[1].Width = 21.00;
+                xlsWorksheet.Columns[2].Width = 18.00;
+                xlsWorksheet.Columns[3].Width = 19.00;
+                xlsWorksheet.Columns[4].Width = 50.00;
+                xlsWorksheet.Columns[5, 8].Width = 18.00;
                 xlsWorksheet.Columns[9].Width = 25;
                 xlsWorksheet.Columns[10].Width = 13.57;
             }
